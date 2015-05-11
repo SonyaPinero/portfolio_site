21ec8c335acb8a99cea9958734fc6f70cd1e543c
Sonya Pinero <sonyapinero@gmail.com>
2015-05-11 13:13:13 -0400

    colors

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,15 +2,10 @@ console.log('index.js loaded')
 $(function() {
 
 
+		$("#sonya-img").fadeIn('slow');
 
-
-
-
-
-
-
-
-
-
+    $("#sonya-img").animate({ 
+        marginLeft: "+=1000px",
+    }, 3000 );
 
 })
