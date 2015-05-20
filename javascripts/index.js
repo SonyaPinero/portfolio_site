84e7bc665dede406d46759cb88c24bc0f18de647
Sonya Pinero <sonyapinero@gmail.com>
2015-05-20 11:11:46 -0400

    adding content with click events

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,14 +2,36 @@ console.log('index.js loaded')
 $(function() {
 
 
-			$("#sonya-img").fadeIn('slow');
+	// $("#sonya-img").fadeIn('slow');
 
-    	$("#sonya-img").delay(3500).animate({ 
-        marginLeft: "+=1000px",
-    }, 3000 );
+ //   $("#sonya-img").delay(3500).animate({ 
+ //        marginLeft: "+=1000px",
+ //    }, 3000 );
 
-	$("#top").animate({height: 125, width: 1200}, 4000);
-	$("#nav").animate({height: 1200, width: 125}, 4000);
+	// $("#top").animate({height: 125, width: 1200}, 4000);
+	// $("#nav").animate({height: 1200, width: 125}, 4000);
+
+
+	var elements = $("#pdf, #contact-info, #projects, #sonya-about");
+	
+
+	$("span").click(function() {
+		if ($(this).attr("id") == "resume") {
+			elements.addClass("invisible");
+			$("#pdf").removeClass("invisible");
+		} else if ($(this).attr("id") == "contact") {
+			elements.addClass("invisible");
+			$("#contact-info").removeClass("invisible");
+		} else if ($(this).attr("id") == "portfolio") {
+			elements.addClass("invisible");
+			$("#projects").removeClass("invisible");
+		}
+	});
+
+
+	// $("#resume").click(function() {
+	// 	$("#pdf").removeClass("invisible");
+	// });
 
 
 })
