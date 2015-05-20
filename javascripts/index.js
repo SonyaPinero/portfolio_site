b4617feffc5d3204c1852475475d5a15390c40dd
Sonya Pinero <sonyapinero@gmail.com>
2015-05-20 12:43:36 -0400

    links working

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,17 +2,17 @@ console.log('index.js loaded')
 $(function() {
 
 
-	// $("#sonya-img").fadeIn('slow');
+	$("#sonya-img").fadeIn('slow');
 
- //   $("#sonya-img").delay(3500).animate({ 
- //        marginLeft: "+=1000px",
- //    }, 3000 );
+   $("#sonya-img").delay(3500).animate({ 
+        marginLeft: "+=1000px",
+    }, 3000 );
 
-	// $("#top").animate({height: 125, width: 1200}, 4000);
-	// $("#nav").animate({height: 1200, width: 125}, 4000);
+	$("#top").animate({height: 125, width: 1200}, 4000);
+	$("#nav").animate({height: 1200, width: 125}, 4000);
 
 
-	var elements = $("#pdf, #contact-info, #projects, #sonya-about");
+	var elements = $("#pdf, #contact-info, #projects, #sonya-about, #default");
 	
 
 	$("span").click(function() {
@@ -25,6 +25,12 @@ $(function() {
 		} else if ($(this).attr("id") == "portfolio") {
 			elements.addClass("invisible");
 			$("#projects").removeClass("invisible");
+		} else if ($(this).attr("id") == "about") {
+			elements.addClass("invisible");
+			$("#sonya-about").removeClass("invisible");
+		} else {
+			elements.addClass("invisible");
+			$("#default").removeClass("invisible");
 		}
 	});
 
