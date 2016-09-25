f32c140939f1f573b465236c24fc385a17adf57e
Sonya <sonyapinero@gmail.com>
2016-09-25 19:00:23 +0000

    push from cloud9 since my personal mac is dying

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const server = express();
 
 server.use(express.static('./build/public'));
 
-server.listen(3000, () => console.log('Listening on port 3000'));
+server.listen(8080, () => console.log('Listening on port 8080'));
