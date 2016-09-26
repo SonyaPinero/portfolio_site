9620a12167908259d704ebcba182f8ec1384083d
Sonya <sonyapinero@gmail.com>
2016-09-26 00:48:51 +0000

    grabbing welcome rect

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import Snap from 'snapsvg'
 
 window.onload = () => {
-       Snap.load('/svg/TimesSquare.svg', (f) => {
-           console.log(f)
-       })
+    //   Snap.load('/svg/TimesSquare.svg', (f) => {
+    //       console.log(f)
+    //   })
+    
+    let times = document.getElementById('TimesSquare');
+    let welcome = times.contentDocument.getElementById("welcomeRect");
+    console.log(welcome)
     
 }
