d2c9dae42ffa54987b7f68f75974c9bd188cc671
Sonya <sonyapinero@gmail.com>
2016-10-02 20:54:33 +0000

    test

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import Snap from 'snapsvg'
 
 window.onload = () => {
-    //   Snap.load('/svg/TimesSquare.svg', (f) => {
-    //       console.log(f)
-    //   })
-    
     let times = document.getElementById('TimesSquare');
-    let welcome = times.contentDocument.getElementById("welcomeRect");
-    console.log(welcome)
+    let timesSVG = times.contentDocument.getElementById('timesSVG');
+    
+    let welcomeRect = timesSVG.getElementById('welcomeRect');
+    console.log(welcomeRect)
+
     
 }
