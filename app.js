e0195ea0f6f2b1a764d4b9d0e28f57fa7e53fdf9
Sonya <sonyapinero@gmail.com>
2016-10-02 23:25:05 +0000

    more ids

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-import Snap from 'snapsvg'
+import Kute from 'kute.js'
 
 window.onload = () => {
+    //grab Times Square SVG from Object
     let times = document.getElementById('TimesSquare');
     let timesSVG = times.contentDocument.getElementById('timesSVG');
+    
+    //make Welcome appear
+    //let welcomeArray = ['WELCOME'].split('')
+    let welcomeW = timesSVG.getElementById('welcomeW');
+    
     
 }
