fc99ff20a406640a9219cba207b18871f920363c
Sonya <sonyapinero@gmail.com>
2016-10-03 00:56:44 +0000

    app

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-import Kute from 'kute.js'
-
 window.onload = () => {
     //grab Times Square SVG from Object
     let times = document.getElementById('TimesSquare');
     let timesSVG = times.contentDocument.getElementById('timesSVG');
     
     //make Welcome appear
-    //let welcomeArray = ['WELCOME'].split('')
+    let welcomeArr = 'WELCOME'.split('')
     let welcomeW = timesSVG.getElementById('welcomeW');
+    
+    welcomeArr.forEach((letter) => {
+        timesSVG.getElementById('welcome' + letter).style.visibility = 'visible';
+    })
     
     
 }
