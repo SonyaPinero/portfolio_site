a5bbcfb7251f7ba985a788b1a5bf7b735ebb2da6
Sonya <sonyapinero@gmail.com>
2016-10-03 02:08:46 +0000

    clean up

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 window.onload = () => {
+    let welcome = document.getElementById('welcomeW')
+    welcome.style.visibility = 'visible'
     let tween = KUTE.to('#welcomeW', { fill: 'red' });
     tween.start()
 }
