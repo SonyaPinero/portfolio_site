579ae2c3f73eafc0a44c0f2dd98419a09f6dcce2
Sonya <sonyapinero@gmail.com>
2016-10-09 21:36:39 +0000

    es6 syntax

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 window.onload = () => {
     
-   function welcomeAppears(){
+   const welcomeAppears = () => {
     return new Promise((resolve, reject) => {
         let index = 0;
         let welcomeArr = 'WELCOMEe'.split('');
     
         let timer = setInterval(() =>{
-        let welcome = document.getElementById('welcome' + welcomeArr[index])
-        welcome.style.visibility = 'visible'
+        let welcome = document.getElementById('welcome' + welcomeArr[index]);
+        welcome.style.visibility = 'visible';
         let tween = KUTE.to(welcome, { fill: '#29B6F6' }).start();
-        index++
+        index++;
         if (index >= welcomeArr.length) {
             clearInterval(timer);
-            resolve(timer)
-         }
+            resolve(timer);
+         };
         }, 250)
-    })
+    });
+   };
+   
+   const linkedinAppears = () =>{
+    return new Promise((resolve, reject) => {
+        console.log('linkedin appearing');
+        resolve();
+    });
    }
+   
+   const sonyaAppears = () =>{
+    return new Promise((resolve, reject) => {
+        console.log('sonya appearing');
+        resolve();  
+    });
+   };
+   
+   
    
    (async function() {
     await welcomeAppears();
+    await linkedinAppears();
+    await sonyaAppears();
     console.log('done')
   })(); 
     
